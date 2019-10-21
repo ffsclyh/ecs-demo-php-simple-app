d04899d1f3f220d1d229a13ba950dc94afc052b1
ffsclyh <44584613+ffsclyh@users.noreply.github.com>
2019-10-21 09:15:58 -0500

    test

--- src/index.php
+++ src/index.php
@@ -15,9 +15,9 @@
     <body>
         <div class="container">
             <div class="hero-unit">
-                <h1>Simple PHP App</h1>
+                <h1>Simple Test Monday PHP App</h1>
                 <h2>Congratulations Everyone  </h2>
-                <p>Nancy AAAAA PHP application is now running on a container in Amazon ECS.</p>
+                <p>Test MOnday AAAAA PHP application is now running on a container in Amazon ECS.</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
                 <?php
                         $myfile = fopen("/var/www/my-vol/date", "r") or die("");
